101b59787d174c6f3d1fd0a7975192f0e4023da8
Steve M <uziandari@gmail.com>
2017-01-08 12:24:09 -0500

    added navigation menu, completeed for mobile view (no style)

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,23 +4,81 @@ import { Link } from 'react-router';
 import './style.css';
 
 export default class Navigation extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowWidth: window.innerWidth,
+      mobileNavVisible: false
+    };
+  }
+
+  handleResize() {
+    this.setState({windowWidth: window.innerWidth});
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize.bind(this));
+  }
+
+  navLinks() {
+    return [
+      <ul className="nav">
+        <li key={1} className="link-item">
+          <Link to="/about">About</Link>
+        </li>
+        <li key={2} className="link-item">
+          <Link to="/timeline">Timeline</Link>
+        </li>
+        <li key={3} className="link-item">
+          <Link to="/works">Works</Link>
+        </li>
+      </ul>
+    ];
+  }
+
+  renderMobileNav() {
+    if(this.state.mobileNavVisible) {
+      return this.navLinks();
+    }
+  }
+
+  handleNavClick() {
+    if(!this.state.mobileNavVisible) {
+      this.setState({mobileNavVisible: true});
+    }
+    else {
+      this.setState({mobileNavVisible: false});
+    }
+  }
+
+  renderNav() {
+    if(this.state.windowWidth <= 1080) {
+      return [
+        <div className="mobile-nav">
+          <p onClick={this.handleNavClick.bind(this)}><i className="icons">view_headline</i></p>
+          {this.renderMobileNav()}
+        </div>
+      ];
+    }
+    else {
+      return [
+        <div key={7} className="nav-menu">
+          {this.navLinks()}
+        </div>
+      ];
+    }
+  }
+
   render() {
     return (
-      <div>
-        <ul className="nav">
-          <li className="link-item">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="link-item">
-            <Link to="/about">About</Link>
-          </li>
-          <li className="link-item">
-            <Link to="/timeline">Timeline</Link>
-          </li>
-          <li className="link-item">
-            <Link to="/works">Works</Link>
-          </li>
-        </ul>
+      <div className="nav-wrapper">
+        <div className="site-title"><Link to="/">TITLE</Link></div>
+        {this.renderNav()}
       </div>
     );
   }
